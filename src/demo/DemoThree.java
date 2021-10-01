64f81cf0fad8f7127fe16df18286bea3016a08ef
Crystal <Crystal@DESKTOP-81QTH9S>
2021-09-30 21:18:02 -0400

    I added my name

--- src/demo/DemoThree.java
+++ src/demo/DemoThree.java
@@ -4,6 +4,11 @@ public class DemoThree {
 	
 	public static void main(String[] args) {
 		System.out.println("Demo Three");
+		
+		
+		String name = "Crystal";
+		
+		System.out.println("My Name is " + name);
 	}
 
 }
